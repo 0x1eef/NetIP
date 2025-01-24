43de4dd9831f9ec4e52e29f3b9d23a16ac138a15
0x1eef <0x1eef@protonmail.com>
2025-01-24 05:21:12 -0300

    Add ${targetdir} build var
    For example:
    targetdir=/var/nfs/ext/NetIP make build

--- build.ts
+++ build.ts
@@ -1,9 +1,19 @@
+import process from "node:process";
+import path from "node:path";
 import { build } from "https://deno.land/x/esbuild/mod.js";
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.1";
 
+function getTargetDir() {
+  if (process.env.targetdir) {
+    return process.env.targetdir;
+  } else {
+    return path.resolve(process.cwd(), "build");
+  }
+}
+
 await build({
   entryPoints: ["src/js/index.tsx"],
-  outfile: "build/js/index.js",
+  outfile: path.resolve(getTargetDir(), "js/index.js"),
   bundle: true,
   format: "esm",
   platform: "browser",
